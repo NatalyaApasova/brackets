6177b1c1375e482fd8e0253ab8f8b330bb0fcba1
Natalya <natalya.gulak.apasova@yandex.by>
2019-03-19 23:17:06 +0300

    feat: Implement function, that for given brackets sequence will return true if it is correct and false otherwise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let stack = [];
+	let open = [];
+	let close = [];
+  	
+	for (i = 0; i < bracketsConfig.length; i++) {
+	 	open.push(bracketsConfig[i][0]);
+ 		close.push(bracketsConfig[i][1]);
+ 	}
+
+	for (i = 0; i < str.length; i++) {
+		let last = stack[stack.length - 1];
+ 		if (open.indexOf(last) !== close.indexOf(str[i]) || stack.length === 0) {
+ 			stack.push(str[i]);
+ 		} else if (open.indexOf(last) === close.indexOf(str[i]) && close.indexOf(str[i]) !== -1 ) {
+ 			stack.pop();
+ 		} else {
+ 			return false;
+ 		}
+ 	}
+ 	if (stack.length) {
+ 		return false;
+ 	}
+ 	return true;
 }
